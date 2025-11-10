abe28dce09c0292dd370a40ea187a822fbedb9fc
shuklasuman0523-stack <shuklasuman0523@gmail.com>
2025-11-10 21:17:16 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/podcast/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
